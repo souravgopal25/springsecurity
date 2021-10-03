ffa732b271f697214961a1bd42b11ff7c4ea9206
Sourav <souravgopal25@gmail.com>
2021-10-03 13:18:43 +0530

    Role Based Authentication +Authorization

--- src/main/java/com/example/springsecurity/springsecurity/SecurityConfiguration.java
+++ src/main/java/com/example/springsecurity/springsecurity/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.springsecurity.springsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,11 +14,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("Sourav").password("1234").roles("USER").
-                and().withUser("foo").password("foo").roles("USER");
+                and().withUser("foo").password("foo").roles("ADMIN");
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().
+             antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN","USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 }
